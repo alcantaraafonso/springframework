488edd5e68b270ef9ffaf60501e4877447c87a5d
Afonso Alcantara <alcantara.afonso@gmail.com>
2023-03-05 19:33:54 -0300

    Adding @Autowired on UserController

--- src/main/java/br/com/beganinha/springframework/controllers/UserController.java
+++ src/main/java/br/com/beganinha/springframework/controllers/UserController.java
@@ -1,6 +1,7 @@
 package br.com.beganinha.springframework.controllers;
 
 import br.com.beganinha.springframework.repository.UsersRepositoryMongodb;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
+    @Autowired
     private UsersRepositoryMongodb usersRepositoryMongodb;
-
-    public UserController(UsersRepositoryMongodb usersRepositoryMongodb) {
-        this.usersRepositoryMongodb = usersRepositoryMongodb;
-    }
 
     @GetMapping("/user")
     public String getUsers(Model model) {
